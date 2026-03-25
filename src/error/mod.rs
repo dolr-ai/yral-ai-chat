f7468f528955ddf281e1556fe35f07b09f537311
Ravi Sawlani <ravisawlani04@gmail.com>
2026-03-25 20:23:53 +0530

    capture errors irrespective of their status code

--- src/error/mod.rs
+++ src/error/mod.rs
@@ -78,9 +78,7 @@ impl AppError {
 impl IntoResponse for AppError {
     fn into_response(self) -> Response {
         let (status, code) = self.status_and_code();
-        if status.is_server_error() {
-            sentry::capture_error(&self);
-        }
+        sentry::capture_error(&self);
         let body = ErrorBody {
             error: code,
             message: self.to_string(),
